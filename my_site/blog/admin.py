8aa461c26b5ec3ba0d491f399df91437698303af
Petar Koprinkov <petur_koprinkov@abv.bg>
2024-08-14 18:42:43 +0300

    added database

--- my_site/blog/admin.py
+++ my_site/blog/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Author, Post, Tag
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name')
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author__last_name', 'slug')
+    prepopulated_fields = {'slug': ('title',)}
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('caption',)
--- my_site/blog/models.py
+++ my_site/blog/models.py
@@ -1,3 +1,59 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
-# Create your models here.
+
+class Author(models.Model):
+    first_name = models.CharField(
+        max_length=50
+    )
+
+    last_name = models.CharField(
+        max_length=50
+    )
+
+    email = models.EmailField()
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+
+class Post(models.Model):
+    title = models.CharField(
+        max_length=50
+    )
+    excerpt = models.TextField()
+
+    image_name = models.CharField(
+        max_length=50
+    )
+
+    date = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    slug = models.SlugField(
+        unique=True,
+    )
+
+    content = models.TextField()
+
+    author = models.ForeignKey(
+        to=Author,
+        on_delete=models.CASCADE,
+        related_name='posts'
+    )
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+
+class Tag(models.Model):
+    caption = models.CharField(
+        max_length=50
+    )
+
+    posts = models.ManyToManyField(
+        to=Post,
+        related_name='tags'
+    )
